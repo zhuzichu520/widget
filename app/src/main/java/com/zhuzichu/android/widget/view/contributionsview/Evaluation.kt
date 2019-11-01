c0650b4ade2c20fc5d397a75f2393a9c97361386
zhuzichu <coffee.zhu@jollycorp.com>
2019-11-01 11:47:05 +0800

    update

--- app/src/main/java/com/zhuzichu/android/widget/view/contributionsview/Evaluation.kt
+++
@@ -1,9 +0,0 @@
-package com.zhuzichu.android.widget.view.contributionsview
-
-enum class Evaluation {
-    E,
-    D,
-    C,
-    B,
-    A
-}
--- app/src/main/java/com/zhuzichu/android/widget/view/contributionsview/InsufficientNumberOfMapElementsException.kt
+++
@@ -1,6 +0,0 @@
-package com.zhuzichu.android.widget.view.contributionsview
-
-class InsufficientNumberOfMapElementsException(private val amount: Int) : RuntimeException() {
-    override val message: String?
-        get() = "Expected number of map is 5. But it was $amount"
-}
