b9c91caf4d7e76e55402805764487571fd44266c
zhuzichu <zhuzichu520@gmail.com>
2019-11-13 20:52:37 +0800

    update

---
+++ app/src/main/java/com/zhuzichu/android/widget/sharp/SvgParseException.java
@@ -0,0 +1,19 @@
+package com.zhuzichu.android.widget.sharp;
+
+public class SvgParseException extends RuntimeException {
+
+    @SuppressWarnings("unused")
+    public SvgParseException(String s) {
+        super(s);
+    }
+
+    @SuppressWarnings("unused")
+    public SvgParseException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public SvgParseException(Throwable throwable) {
+        super(throwable);
+    }
+
+}
